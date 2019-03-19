1c20c42ef80b0c8ad5a2b476cfe202a2d04ab8e7
huangyongsheng <yshhuang@foxmail.com>
2019-03-19 18:02:05 +0800

    drift

---
+++ source-code/src/app/aitong/aitong.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlbumComponent } from './album/album.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AlbumComponent]
+})
+export class AitongModule { }
--- source-code/src/app/aitong/album/album.component.spec.ts
+++ source-code/src/app/aitong/album/album.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SortComponent } from './sort.component';
+import { AlbumComponent } from './album.component';
 
-describe('SortComponent', () => {
-  let component: SortComponent;
-  let fixture: ComponentFixture<SortComponent>;
+describe('AlbumComponent', () => {
+  let component: AlbumComponent;
+  let fixture: ComponentFixture<AlbumComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SortComponent ]
+      declarations: [ AlbumComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SortComponent);
+    fixture = TestBed.createComponent(AlbumComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
